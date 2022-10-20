ab13b333a320153f92744c14b5e4e706e8d266cb
Eleen <109869625+Eleen-228@users.noreply.github.com>
2022-10-20 13:29:54 -0400

    Uploaded my portfolio page

---
+++ index.js
@@ -0,0 +1,26 @@
+window.addEventListener("scroll", reveal);
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var n = 0; n < reveals.length; n++) {
+    var windowHeight = window.innerHeight;
+    // console.log(`windowheight: ${windowHeight}`);
+    var revealTop = reveals[n].getBoundingClientRect().top;
+    // console.log(`revealtop: ${revealTop}`);
+    let revealPoint = 150;
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[n].classList.add("active");
+    } else {
+      reveals[n].classList.remove("active");
+    }
+  }
+}
+
+// Preloader Js
+let loader = document.querySelector(".loader");
+window.addEventListener("load", vanish);
+function vanish() {
+  setTimeout(() => {
+    loader.classList.add("disappear");
+    document.body.classList.remove("scrollDisabled");
+  }, 6500);
+}
