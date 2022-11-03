28040605d9c7d3a9c2ea9a305f59836acf73540e
Eleen-228 <109869625+Eleen-228@users.noreply.github.com>
2022-11-03 09:51:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-window.addEventListener("scroll", reveal);
+window.addEventListener('scroll', reveal);
 function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
+  var reveals = document.querySelectorAll('.reveal');
   for (var n = 0; n < reveals.length; n++) {
     var windowHeight = window.innerHeight;
     // console.log(`windowheight: ${windowHeight}`);
@@ -8,19 +8,19 @@ function reveal() {
     // console.log(`revealtop: ${revealTop}`);
     let revealPoint = 150;
     if (revealTop < windowHeight - revealPoint) {
-      reveals[n].classList.add("active");
+      reveals[n].classList.add('active');
     } else {
-      reveals[n].classList.remove("active");
+      reveals[n].classList.remove('active');
     }
   }
 }
 
 // Preloader Js
-let loader = document.querySelector(".loader");
-window.addEventListener("load", vanish);
+let loader = document.querySelector('.loader');
+window.addEventListener('load', vanish);
 function vanish() {
   setTimeout(() => {
-    loader.classList.add("disappear");
-    document.body.classList.remove("scrollDisabled");
+    loader.classList.add('disappear');
+    document.body.classList.remove('scrollDisabled');
   }, 6500);
 }
